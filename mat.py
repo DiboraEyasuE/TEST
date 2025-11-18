81ea9bdd8bf9780512230d9c098be655277e68b9
DiboraEyasuE <diboraeyasu792@gmail.com>
2025-11-18 22:56:54 +0300

    feat: added a color and font size to labels

--- mat.py
+++ mat.py
@@ -7,4 +7,7 @@ days = [1, 2, 3, 4, 5, 6, 7]
 temperature = [72, 75, 68, 70, 74, 77, 71]
 
 plt.plot(days, temperature)
+plt.title("Weekly Temperature", fontsize=15)
+plt.xlabel("Days of the week", fontsize=12, color='blue')
+plt.ylabel("Temperature(F)", fontsize=12, color='blue')
 plt.show()
